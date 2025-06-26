65c9efe059b8dff07c7b12a9f274484ed455ed28
gabrielkomor <gabkomti@gmail.com>
2025-06-26 12:53:31 +0200

    adding bullet entity tests cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from unittest.mock import Mock
 import scripts.bar_class as bar
 import scripts.bullet_class as bullet_class
+import scripts.bullet_entity_class as bullet_entity_class
+import scripts.entity_class as entity_class
 
 
 @pytest.fixture
@@ -33,3 +35,15 @@ def mock_image():
 def mock_bullet(monkeypatch, mock_player, mock_image):
     monkeypatch.setattr('pygame.image.load', lambda path: mock_image)
     return bullet_class.Bullet(mock_player)
+
+
+@pytest.fixture
+def mock_entity(mock_image, monkeypatch):
+    monkeypatch.setattr('pygame.image.load', lambda path: mock_image)
+    return entity_class.Entity(50, 50)
+
+
+@pytest.fixture
+def mock_bullet_entity(mock_entity, monkeypatch, mock_image):
+    monkeypatch.setattr('pygame.image.load', lambda path: mock_image)
+    return bullet_entity_class.BulletEntity(mock_entity, 5)
---
+++ tests/test_bullet_entity.py
@@ -0,0 +1,34 @@
+import pytest
+from unittest.mock import Mock
+
+
+@pytest.mark.parametrize('counter, result, y_cord', [(2, 55, 55), (3, 45, 55), (7, 45, 55), (10, 55, 55)])
+def test_tick(mock_bullet_entity, mock_player, counter, result, y_cord):
+    entity_bullets = []
+    explosions = []
+    mock_bullet_entity.counter = counter
+    mock_bullet_entity.x_cord = 50
+    mock_bullet_entity.y_cord = 50
+    mock_bullet_entity.speed = 5
+    mock_bullet_entity.tick(mock_bullet_entity, entity_bullets, mock_player, explosions)
+    assert mock_bullet_entity.x_cord == result
+    assert mock_bullet_entity.y_cord == y_cord
+
+
+def test_remove_entity(mock_bullet_entity, mock_player, monkeypatch):
+    entity_bullets = [mock_bullet_entity]
+    explosions = []
+    mock_bullet_entity.y_cord = 200
+    mock_bullet_entity.height = 10
+    monkeypatch.setattr('scripts.settings_class.Settings.window_height', 250)
+    mock_bullet_entity.tick(mock_bullet_entity, entity_bullets, mock_player, explosions)
+    assert mock_bullet_entity not in entity_bullets
+
+
+def test_draw(mock_bullet_entity):
+    window = Mock()
+    mock_bullet_entity.draw(window)
+    assert window.blit.call_count == 1
+    args, kwargs = window.blit.call_args
+    assert args[0] == mock_bullet_entity.image
+    assert args[1] == (mock_bullet_entity.x_cord, mock_bullet_entity.y_cord)
